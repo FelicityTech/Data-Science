39f374b1ca7291a95ebfacf55cc5321e87c9f120
FelicityTech <souceking@gmail.com>
2024-07-26 12:36:12 +0100

    distribution and histogram

--- pandas/distribution_vis.py
+++ pandas/distribution_vis.py
@@ -1,9 +1,9 @@
 import numpy as np
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 
 np.random.seed(123)
 final_tails = []
-for x in range(100):
+for x in range(10000):
     tails = [0]
     for x in range(10):
         coin = np.random.randint(0, 2)
@@ -11,4 +11,5 @@ for x in range(100):
 
     final_tails.append(tails[-1])
 
-    
+plt.hist(final_tails, bins=10)
+plt.show()
