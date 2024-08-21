f1d3a91cc5e8fdddd477c1a87febd63791289aac
FelicityTech <souceking@gmail.com>
2024-08-21 16:44:24 +0100

    Avocado supply and demand

--- Data-Manipulation-with-pandas/avocado_supply_demand.py
+++ Data-Manipulation-with-pandas/avocado_supply_demand.py
@@ -1,5 +1,5 @@
 # Scatter plot of avg_price vs. nb_sold with title
-avocados.plot(x="nb_sold", y="avg_price", kind="scatter")
+avocados.plot(x="nb_sold", y="avg_price", kind="scatter", title="Avocado supply and demand")
 
 # Show the plot
 plt.show()
