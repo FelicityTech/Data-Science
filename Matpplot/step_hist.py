f8a37d3685438454c1a2efc5b740361e275893dc
FelicityTech <souceking@gmail.com>
2025-08-04 23:13:00 +0100

    Step histogram

--- Matpplot/step_hist.py
+++ Matpplot/step_hist.py
@@ -1,10 +1,10 @@
 fig, ax = plt.subplots()
 
 # Plot a histogram of "Weight" for mens_rowing
-ax.hist(mens_rowing["Weight"], label="Rowing")
+ax.hist(mens_rowing["Weight"], label="Rowing", histtype='step', bins=5)
 
 # Compare to histogram of "Weight" for mens_gymnastics
-ax.hist( mens_gymnastics["Weight"], label="Gymnastics")
+ax.hist( mens_gymnastics["Weight"], label="Gymnastics", histtype='step', bins=5)
 
 ax.set_xlabel("Weight (kg)")
 ax.set_ylabel("# of observations")
