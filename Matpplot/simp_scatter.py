53fea54bffbc9ffe3ec196e93356fe56f542e5f8
FelicityTech <souceking@gmail.com>
2025-08-05 08:04:44 +0100

    Simple scatter plot

--- Matpplot/simp_scatter.py
+++ Matpplot/simp_scatter.py
@@ -4,7 +4,7 @@ fig, ax = plt.subplots()
 ax.scatter(climate_change["co2"], climate_change["relative_temp"])
 
 # Set the x-axis label to "CO2 (ppm)"
-ax.set_xlabel("co2 (ppm)")
+ax.set_xlabel("CO2 (ppm)")
 
 # Set the y-axis label to "Relative temperature (C)"
 ax.set_ylabel("Relative temperature (C)")
