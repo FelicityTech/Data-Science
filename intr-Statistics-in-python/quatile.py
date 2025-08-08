22c3fd27ea35b9feca3ff636be80df348e0731e7
FelicityTech <souceking@gmail.com>
2025-08-08 14:34:04 +0100

    Quartiles

--- intr-Statistics-in-python/quatile.py
+++ intr-Statistics-in-python/quatile.py
@@ -1,2 +1,2 @@
 # Calculate the quartiles of co2_emission
-print(np.quantile(food_consumption['co2_emission'], [0.25,0.5,  0.75]))
+print(np.quantile(food_consumption['co2_emission'], [0, 0.25, 0.5, 0.75, 1]))
